4c27c3ee770a3dc89f40a39e4ec74380e4b9d794
Yermekov Aibar <160598362+F1awless77@users.noreply.github.com>
2025-05-16 21:39:27 +0600

    feat: Profile editing

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import {
   Award,
@@ -8,9 +8,12 @@ import {
   CalendarDays,
   Trash,
   CircleAlert,
+  Pencil,
 } from "lucide-react";
 import axios from "axios";
 import { formatDistanceToNowStrict } from "date-fns";
+import EditImage from "@/components/EditImage";
+import { useAppSelector } from "@/hooks/hooks";
 
 interface IUser {
   id: number | string;
@@ -36,10 +39,11 @@ interface IActivity {
 const ProfilePage = () => {
   const { id } = useParams();
   const userId = id ? parseInt(id) : null;
+  const { user } = useAppSelector((state) => state.user);
 
   const [userData, setUserData] = useState<null | IUser>(null);
   const [username, setUsername] = useState(userData?.username);
-  const [avatar, setAvatar] = useState<string>("");
+  const [avatar, setAvatar] = useState<File | null | string>(null);
   const [email, setEmail] = useState(userData?.email);
   const [createdAt, setCreatedAt] = useState<Date | undefined>(undefined);
   const [points, setPoints] = useState<number>(0);
@@ -47,6 +51,12 @@ const ProfilePage = () => {
   const [position, setPosition] = useState<number>(0);
 
   const [activities, setActivities] = useState([]);
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [isAvatarEdit, setIsAvatarEdit] = useState(false);
+  const [isUsernameEdit, setIsUsernameEdit] = useState(false);
+
+  const [usernameErr, setUsernameErr] = useState("");
 
   const formatDate = (date: Date): string => {
     const day = String(date.getDate()).padStart(2, "0");
@@ -92,6 +102,57 @@ const ProfilePage = () => {
     }
   };
 
+  const handleAvatarChange = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    if (!avatar) {
+      return alert("Select an image");
+    }
+
+    formData.append("avatar", avatar);
+
+    try {
+      await axios.patch(
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/${id}/edit_avatar`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      setIsAvatarEdit(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      window.location.reload();
+    }
+  };
+
+  const handleUsernameChange = async () => {
+    if (!username) {
+      return alert("Type in a new username");
+    }
+
+    try {
+      await axios.patch(
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/${id}/edit_username`,
+        {
+          username: username,
+        }
+      );
+
+      setIsUsernameEdit(false);
+      window.location.reload();
+    } catch (error: any) {
+      console.log(error);
+      setUsernameErr(error.response?.data?.message);
+    }
+  };
+
   useEffect(() => {
     const fetch = async () => {
       try {
@@ -118,15 +179,47 @@ const ProfilePage = () => {
   return (
     <div className="pt-32 md:pt-20 px-4 md:px-6 min-h-screen">
       <div className="max-w-4xl mx-auto">
+        {isAvatarEdit && user?.id == id ? (
+          <EditImage
+            handleSave={handleAvatarChange}
+            image={avatar}
+            setIsEdit={setIsAvatarEdit}
+            setImage={setAvatar}
+          />
+        ) : (
+          ""
+        )}
+
+        {user?.id == id ? (
+          <button
+            onClick={() => setIsEdit((prev) => !prev)}
+            className="p-2 px-4 mb-4 bg-black dark:bg-darkSecondary text-white text-sm md:text-base font-medium rounded-full"
+          >
+            {isEdit ? "Cancel" : "Edit Profile"}
+          </button>
+        ) : (
+          ""
+        )}
         <div className="bg-dark-secondary rounded-2xl p-6 md:p-8 shadow-lg">
           <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
             {/* Ava */}
             <div className="relative">
               <img
-                src={avatar}
+                src={userData?.avatar}
                 alt={username}
                 className="w-32 h-32 rounded-full object-cover border-4 border-primary-green/30"
               />
+              {isEdit && user?.id == id ? (
+                <div className="absolute top-0 right-0 hover:opacity-50 p-2 rounded-full bg-primary-green">
+                  <Pencil
+                    onClick={() => setIsAvatarEdit(true)}
+                    size={20}
+                    className=""
+                  />
+                </div>
+              ) : (
+                ""
+              )}
               <div className="absolute -bottom-2 -right-2 bg-primary-green rounded-full p-2">
                 <Award className="w-6 h-6 text-black" />
               </div>
@@ -134,7 +227,55 @@ const ProfilePage = () => {
 
             {/* Info */}
             <div className="flex-1 text-center md:text-left">
-              <h1 className="text-3xl font-bold text-white mb-2">{username}</h1>
+              {isUsernameEdit ? (
+                <label className="w-full">
+                  <small className="uppercase text-zinc-500 font-semibold text-xs mt-3">
+                    Username:
+                  </small>
+                  {usernameErr.length > 0 ? (
+                    <p className="text-red-500">{usernameErr}</p>
+                  ) : (
+                    ""
+                  )}
+                  <input
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    type="text"
+                    className="w-full text-white text-sm md:text-xl font-bold mb-3 p-2 outline-none border-b rounded-md"
+                  />
+                  <div className="flex items-center justify-end gap-2">
+                    <button
+                      onClick={() => {
+                        setIsUsernameEdit(false);
+                      }}
+                      className="py-2 px-4 rounded-2 rounded-3xl hover:bg-dark-color"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      onClick={() => handleUsernameChange()}
+                      className="py-2 px-8 rounded-2 rounded-3xl bg-dark-color text-white hover:opacity-75"
+                    >
+                      Save Changes
+                    </button>
+                  </div>
+                </label>
+              ) : (
+                <div className="flex gap-2">
+                  <h1 className="text-3xl font-bold text-white mb-2">
+                    {userData?.username}
+                  </h1>
+                  {isEdit && user?.id == id ? (
+                    <Pencil
+                      onClick={() => setIsUsernameEdit(true)}
+                      size={20}
+                      className="hover-opacity-50"
+                    />
+                  ) : (
+                    ""
+                  )}
+                </div>
+              )}
 
               <div className="flex flex-wrap flex-col md:justify-start gap-4 text-sm text-gray-400">
                 <div className="flex items-center gap-2">
